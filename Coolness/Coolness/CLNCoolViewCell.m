155c1a9696888470f190c733d033de2392ced6da
Jonathan Lehr <jonathan@aboutobjects.com>
2020-11-04 12:59:28 -0500

    Wednesday WiP

--- Coolness/Coolness/CLNCoolViewCell.m
+++ Coolness/Coolness/CLNCoolViewCell.m
@@ -3,12 +3,42 @@
 
 #import "CLNCoolViewCell.h"
 
+const UIEdgeInsets CLNTextInsets = {
+   .top = 7, .bottom = 8, .left = 12, .right = 12
+};
+
 @interface CLNCoolViewCell ()
 @property (getter=isHighlighted, nonatomic) BOOL highlighted;
 @property (class, readonly, nonatomic) NSDictionary *textAttributes;
 @end
 
 @implementation CLNCoolViewCell
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self == nil) return nil;
+    
+    [self configureLayer];
+    [self configureGestureRecognizer];
+    
+    return self;
+}
+
+// TODO: Override initWithCoder:
+
+- (void)configureGestureRecognizer {
+    UITapGestureRecognizer *recognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(bounce)];
+    recognizer.numberOfTapsRequired = 2;
+    [self addGestureRecognizer:recognizer];
+}
+
+- (void)configureLayer {
+    self.layer.borderWidth = 3;
+    self.layer.borderColor = UIColor.whiteColor.CGColor;
+    self.layer.cornerRadius = 10;
+    self.layer.masksToBounds = YES;
+}
 
 + (NSDictionary *)textAttributes {
     // TODO: Cache an instance in a threadsafe manner.
@@ -21,11 +51,41 @@
     self.alpha = highlighted ? 0.5 : 1.0;
 }
 
+- (void)setText:(NSString *)text {
+    _text = [text copy];
+    [self sizeToFit];
+}
+
+// MARK: - Animation
+
+- (void)bounce {
+    NSLog(@"In %s", __func__);
+    [self animateBounceWithDuration:1 size:CGSizeMake(120, 240)];
+}
+
+- (void)animateBounceWithDuration:(NSTimeInterval)duration size:(CGSize)size {
+    [UIView animateWithDuration:duration
+                     animations:^{
+        [UIView setAnimationRepeatCount:4];
+        [UIView setAnimationRepeatAutoreverses:YES];
+        CGAffineTransform translation = CGAffineTransformMakeTranslation(size.width, size.height);
+        self.transform = CGAffineTransformRotate(translation, M_PI_2);
+    }];
+}
+
+
 // MARK: - Drawing and resizing
 
+- (CGSize)sizeThatFits:(CGSize)size {
+    CGSize newSize = [self.text sizeWithAttributes:self.class.textAttributes];
+    newSize.width += CLNTextInsets.left + CLNTextInsets.right;
+    newSize.height += CLNTextInsets.top + CLNTextInsets.bottom;
+    return newSize;
+}
+
 - (void)drawRect:(CGRect)rect {
-    // TODO: Calculate origin and configure attributes.
-    [self.text drawAtPoint:CGPointZero withAttributes:self.class.textAttributes];
+    CGPoint origin = CGPointMake(CLNTextInsets.left, CLNTextInsets.top);
+    [self.text drawAtPoint:origin withAttributes:self.class.textAttributes];
 }
 
 // MARK: - UIResponder methods
