4e293d09ccdafd1058fea1162ba9b071faac7901
Jonathan Lehr <jonathan@aboutobjects.com>
2020-11-06 11:08:55 -0500

    Data source demo

--- ReadingList/ReadingList/ReadingListController.m
+++ ReadingList/ReadingList/ReadingListController.m
@@ -13,4 +13,19 @@
     
 }
 
+// MARK: UITableViewDataSource protocol methods
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 100; // FIXME: compute value
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Book Cell"];
+//    if (cell == nil) {
+//        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"Book Cell"];
+//    }
+    cell.textLabel.text = [NSString stringWithFormat:@"Row %@", @(indexPath.row + 1)];
+    return cell;
+}
+
 @end
