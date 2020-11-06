ed4617ecfcce08095706492017778d7801cca7e8
Jonathan Lehr <jonathan@aboutobjects.com>
2020-11-06 10:12:05 -0500

    Added ReadingList project.

---
+++ ReadingList/ReadingList/ReadingListController.m
@@ -0,0 +1,16 @@
+// Copyright (C) 2020 About Objects, Inc. All Rights Reserved.
+// See LICENSE.txt for this project's licensing information.
+
+#import "ReadingListController.h"
+
+@implementation ReadingListController
+
+- (IBAction)done:(UIStoryboardSegue *)unwindSegue {
+    // TODO: refresh the UI and save the model
+}
+
+- (IBAction)cancel:(UIStoryboardSegue *)unwindSegue {
+    
+}
+
+@end
